51095540f1871c56d6fbec79ecbfbe784875981d
Oliver Earl <ole4@aber.ac.uk>
2015-11-22 02:34:09 +0000

    TaskerMan stub files added

---
+++ src/php/TaskerMAN/meta.php
@@ -0,0 +1,12 @@
+<?php
+//======================================================================
+// Metadata
+//======================================================================
+?>
+<!-- Metadata -->
+<meta charset = "utf-8" />
+<meta name = "author" content = "Group 5 - Aberystwyth University" />
+<meta name = "description" content = "TaskerMan Task Managing Software" />
+<!-- Includes -->
+<link href = "css/style.css" rel = "stylesheet" />
+<!-- Scripts -->
