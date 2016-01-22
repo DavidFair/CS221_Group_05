394d8de0353285a2ee7826290d5600f4f5912e08
jod32-aber <jod32@aber.ac.uk>
2016-01-22 23:32:30 +0000

    Added Classes for the Connection Settings Window. Currently empty.

---
+++ src/java/src/uk/ac/aber/cs221/group5/gui/ConnSettingsWindow.java
@@ -0,0 +1,5 @@
+package uk.ac.aber.cs221.group5.gui;
+
+public class ConnSettingsWindow {
+
+}
---
+++ src/java/src/uk/ac/aber/cs221/group5/gui/ConnSettingsWindowGUI.java
@@ -0,0 +1,43 @@
+package uk.ac.aber.cs221.group5.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ConnSettingsWindowGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ConnSettingsWindowGUI window = new ConnSettingsWindowGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ConnSettingsWindowGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
