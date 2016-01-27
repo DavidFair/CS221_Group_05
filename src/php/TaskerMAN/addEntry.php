04311fe421902ef8b44dc2ebb516a712bc2b0140
unknown <jee17@PSVWS101.pau.local>
2016-01-27 15:06:00 +0000

    addEntry updated nearly ready for input to databse needs more validation

--- src/php/TaskerMAN/addEntry.php
+++ src/php/TaskerMAN/addEntry.php
@@ -1,9 +1,18 @@
 <?php
 require_once('init.php');
-
-	$result = $pdo->prepare("select * from tbl_tasks");
-	$pdo->execute;
-	$num_rows = $pdo->prepare("mysql_num_rows($result)");
-	$pdo->execute;
-	$row = $num_rows->fetch(PDO::FETCH_OBJ);
-	echo $row;
+    //Finding the last TaskID and adding one to it for the next task to be added
+	$stmt = $pdo->prepare("SELECT TaskID FROM tbl_tasks ORDER BY TaskID DESC LIMIT 1");
+	$stmt->execute();
+	$result = $stmt -> fetch(PDO::FETCH_ASSOC);
+    $taskID = $result['TaskID'];
+    $taskID++;
+    //echo $taskID;
+    echo $_POST["taskName"];
+    echo $_POST["taskAllocated"];
+    echo $_POST["startDate"];
+    echo $_POST["endDate"];
+    //preparing the statement to be added to the database and binding the variables
+    $stmt = $pdo->prepare("INSERT INTO tbl_tasks ('TaskID', 'TaskName', 'StartDate', 'EndDate', 'Status', 'TaskerOwner')
+    VALUES (:taskID, :taskName, :startDate, :endDate, :status, :taskOwner)");
+    $stmt->bindParam(':taskID', $taskID);
+    $stmt->bindParam(':taskName', $_POST["taskName"]);
