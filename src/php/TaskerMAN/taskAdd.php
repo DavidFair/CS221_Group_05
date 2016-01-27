3d9f4f50daa22d436ef5b9d318fc21f577953b01
unknown <tma1@PSVWS064.pau.local>
2016-01-27 16:31:19 +0000

    Edits to the CSS of the Add interface, minor changes to code within taskerAdd to ensure CSS compatability.

--- src/php/TaskerMAN/taskAdd.php
+++ src/php/TaskerMAN/taskAdd.php
@@ -2,30 +2,33 @@
     <div>
         <a href="#close" title="Close" class="close">X</a>
         <?php echo'<h2>'.APP_NAME . ' ' . APP_VER . '</h2>';?>
-        <fieldset>
+        <fieldset id="info_box">
 
 	  <form action="addEntry.php" method="post">
-            <div class="modalLeft">
+            <div class="modal">
                 <label for="taskName">Task Name:</label>
-                <input name="taskName" type="text" required />
+                <input name="taskName" type="text" required /><br/>
+				
                 <label for="taskAllocated">Allocated:</label>
-                <input name="taskAllocated" type="text" required />
+                <input name="taskAllocated" type="text" required /><br/>
+				
                 <label for="assignedTaskMember">Allocated Task Member</label>
                 <select name="taskMember">
                     <option value="email1">David Fairbrother</option>
                     <option value="email2">Joshua Doyale</option>
                 </select><br/>
+				
                 <label for="startDate">Start Date:</label>
-                <input name="startDate" type="date" required />
+                <input name="startDate" type="date" required /><br/>
+				
                 <label for="endDate">End Date:</label>
-                <input name="endDate" type="date" required />
-            </div>
-            <div class="modalRight">
-                <label for="taskDescription">Task Description:</label>
+                <input name="endDate" type="date" required /><br/>
+           
+                <label for="taskDescription">Task Description:</label><br/>
                 <textarea name="taskDescription" rows=8 cols=20 required>
                 </textarea>
                 <br/>
-                <input name="submit" class="modalButton" type="submit" value="Submit" />
+				<input  name="submit" class="modalButton" type="submit" value="Submit" />
                 <input name="clear" class="modalButton" type="reset" value="Clear" />
             </div>
             <input name="taskToAdd" type="hidden" />
