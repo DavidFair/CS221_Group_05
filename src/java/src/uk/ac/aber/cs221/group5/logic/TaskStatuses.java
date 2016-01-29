91cc2d3c05ba7b5f6bbe9ea0eaafe162a7a6c9e9
jod32-aber <jod32@aber.ac.uk>
2016-01-29 10:11:04 +0000

    Added JavaDoc to TaskStatuses

--- src/java/src/uk/ac/aber/cs221/group5/logic/TaskStatuses.java
+++ src/java/src/uk/ac/aber/cs221/group5/logic/TaskStatuses.java
@@ -1,3 +1,15 @@
+/**
+ * This enum details the 3 possible Task Statuses
+ * 
+ * @author Ben Dudley (bed19)
+ * @author David Fairbrother (daf5)
+ * @author Jonathan Englund (jee17)
+ * @author Josh Doyle (jod32)
+ * 
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
 package uk.ac.aber.cs221.group5.logic;
 
 public enum TaskStatuses {
