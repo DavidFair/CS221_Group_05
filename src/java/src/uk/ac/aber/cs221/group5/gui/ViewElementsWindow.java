6bfdd2f9b28fa49b90946bdd18b3344f16364141
jod32-aber <jod32@aber.ac.uk>
2016-01-29 08:59:20 +0000

    Added JavaDoc to ViewElementsWindow

--- src/java/src/uk/ac/aber/cs221/group5/gui/ViewElementsWindow.java
+++ src/java/src/uk/ac/aber/cs221/group5/gui/ViewElementsWindow.java
@@ -1,3 +1,15 @@
+/**
+ * This Class creates the View Elements Window GUI.
+ * 
+ * @author Ben Dudley (bed19)
+ * @author David Fairbrother (daf5)
+ * @author Jonathan Englund (jee17)
+ * @author Josh Doyle (jod32)
+ * 
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
 package uk.ac.aber.cs221.group5.gui;
 
 import java.io.IOException;
@@ -6,6 +18,12 @@ public class ViewElementsWindow {
 
    private ViewElementsWindowGUI childWindow;
 
+   /**
+    * Constructor for the View Elements Window. Passes through the index in the Task List of the Task
+    * that was selected in Main Window.
+    * @param tableRow The index in the Task List of the Task that was selected to View Elements.
+    * @throws IOException if the local Task save file cannot be read.
+    */
    public ViewElementsWindow(int tableRow) throws IOException {
       childWindow = new ViewElementsWindowGUI(tableRow);
    }
